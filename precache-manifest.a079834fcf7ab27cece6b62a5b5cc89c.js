d4e4926862724596d1415b31536e771066fe67fa
박창준 <ckdwns9121@naver.com>
2020-09-27 23:24:41 +0900

    Updates

---
+++ precache-manifest.a079834fcf7ab27cece6b62a5b5cc89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d37fd2665b1ea08a9790b227cc9f599",
+    "url": "/juso-api/index.html"
+  },
+  {
+    "revision": "d1f8a7d12ad4bd926ffd",
+    "url": "/juso-api/static/css/main.f13ff631.chunk.css"
+  },
+  {
+    "revision": "ed5c549e9a122cd7d5a0",
+    "url": "/juso-api/static/js/2.0e39cf61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/juso-api/static/js/2.0e39cf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f8a7d12ad4bd926ffd",
+    "url": "/juso-api/static/js/main.7c2c76ef.chunk.js"
+  },
+  {
+    "revision": "8ebadd2283cf3c8da6e2",
+    "url": "/juso-api/static/js/runtime-main.689f59f6.js"
+  }
+]);
